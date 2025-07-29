17e987332d171274e62f19f1770e882c0a31bdbe
ManashR <mahanandmanash91@gmail.com>
2025-07-29 14:18:39 +0530

    change some code for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -65,6 +65,13 @@ function printLocalIP() {
   return null;
 }
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+	});
+}
+
 server.listen(PORT, async () => {
   await connectDB();
   process.env.NODE_ENV === "production" && console.log(`Server is running on http://localhost:${PORT}`);
